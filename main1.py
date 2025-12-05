2ca324437ec6969953a6c7dcc2c715f49e749df0
Mark <mark.baz@mail.ru>
2025-12-06 00:16:15 +0300

    Изменен поиск максимума на поиск минимума

--- main1.py
+++ main1.py
@@ -1,21 +1,21 @@
-def find_max(numbers):
+def find_min(numbers):  # Изменили имя функции
     """
-    Находит максимальное число в списке.
+    Находит минимальное число в списке.  # Изменили описание
     
     Args:
         numbers (list): Список чисел
         
     Returns:
-        int/float: Максимальное число в списке
+        int/float: Минимальное число в списке  # Изменили возвращаемое значение
     """
     if not numbers:
         return None
     
-    max_number = numbers[0]
+    min_number = numbers[0]  # Изменили название переменной
     for num in numbers:
-        if num > max_number:
-            max_number = num
-    return max_number
+        if num < min_number:  # Изменили знак сравнения
+            min_number = num
+    return min_number
 
 def get_user_list():
     """
@@ -50,15 +50,15 @@ def get_user_list():
     return numbers
 
 if __name__ == "__main__":
-    print("=== Программа поиска максимального числа ===")
+    print("=== Программа поиска минимального числа ===")  # Изменили заголовок
     
     # Получаем список от пользователя
     user_numbers = get_user_list()
     
     if user_numbers:
-        # Находим максимум
-        result = find_max(user_numbers)
+        # Находим минимум  # Изменили комментарий
+        result = find_min(user_numbers)  # Изменили вызов функции
         print(f"\nСписок чисел: {user_numbers}")
-        print(f"Максимальное число: {result}")
+        print(f"Минимальное число: {result}")  # Изменили вывод
     else:
         print("Не удалось получить список чисел.")
